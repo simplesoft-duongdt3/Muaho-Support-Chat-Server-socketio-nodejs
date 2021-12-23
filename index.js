be21f27b082b3e2a6f3a63b3e66c0a2d1a6a19b2
duongdt <doanthanhduong11031990@gmail.com>
2021-12-23 21:01:10 +0700

    refactor socket + user chat UI

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ import { SqliteMessageStore, Message } from './message_store.js';
 import jwt from 'jsonwebtoken';
 import { Server } from "socket.io";
 
+// Rooms
+const cs_room = "cs_room";
+const open_chat_session_cs_room = "open_chat_session_cs_room";
+const open_chat_session_users_room = "open_chat_session_users_room";
+
+//Emit event
+const emit_event_remove_chat_session_user = 'remove_chat_session_user';
+const emit_event_new_chats = 'new_chats';
+const emit_event_open_chat_session_success = 'open_chat_session_success';
+const emit_event_open_session_users = 'open_session_users';
+const emit_event_add_chat_session_user = 'add_chat_session_user';
+const emit_event_chat_history = 'chats_history';
+
+//On event
+const on_event_chat = 'chat';
+const on_event_open_chat_session = 'open_chat_session';
+const on_event_close_chat_session = 'close_chat_session';
+
 async function main() {
   const app = express();
   const server = createServer(app);
@@ -54,83 +72,37 @@ async function main() {
   io.of("/").adapter.on("join-room", (room, id) => {
     console.log(`socket ${id} has joined room ${room}`);
   });
+
   io.of("/").adapter.on("leave-room", (room, id) => {
     var socketTicket = io.sockets.sockets.get(id);
     console.log(`socket ${id} has leaved room ${room} userId ${socketTicket.userId}`);
-    io.to("room_cs").emit('remove_ticket_user', { userId: socketTicket.userId });
+    
+    if(open_chat_session_users_room == room) {
+      io.to(cs_room).emit(emit_event_remove_chat_session_user, { userId: socketTicket.userId });
+    }
   });
 
   io.on('connection', async (socket) => {
     console.log('connection ' + socket.id);
-    var isCs = checkIsCs(socket)
-
-    if (isCs) {
-      socket.join("room_cs");
-      const sockets = await io.in("open_ticket_users").allSockets();
-      const tickets = new Map();
-
-      for (const socketId of sockets) {
-        var socketTicket = io.sockets.sockets.get(socketId);
-        tickets.set(socketTicket.userId, {
-          userId: socketTicket.userId,
-          userName: socketTicket.userName,
-
-        });
-      }   
-      var arrayTickets = Array.from(tickets.values());
-      io.to(socket.id).emit('old_tickets', arrayTickets);
-      console.log('old_tickets ' + JSON.stringify(tickets));
-    } else {
-      socket.join("room_user_" + socket.userId);
-
-      //latest 50 msg + isMore (query 51 msg) -> client call API to get more msg
-      var count = 50
-      const msgList = await messageStore.findMessagesForUser(socket.userId, count + 1)
-      io.to(socket.id).emit('old_news', {
-        msgList: msgList.slice(0, count + 1),
-        isMore: msgList.length > count
-      })
-    }
-
-    socket.on('chat', (msg) => {
-      console.log('chat ' + socket.id + " msg " + JSON.stringify(msg));
-      var roomId = socket.userId;
-      if (isCs) {
-        roomId = msg.receiverUserId;
-      }
-
-      //client msg id improve UX, check status of sending msg
-      //msg.msg_uid
-
-      var message = new Message(
-        socket.userId, msg.receiverUserId, msg.msg, roomId
-      );
-
-      messageStore.saveMessage(message);
-      io.to("room_user_" + roomId).to("room_cs").emit('news', [message]);
+    
+    socket.on(on_event_chat, (msg) => {
+      on_event_chat_handler(checkIsCs, socket, msg, messageStore, io);
     });
 
-    socket.on('open_ticket', (ticket) => {
-      console.log('open_ticket ' + socket.id + " ticket " + JSON.stringify(ticket));
-      var isCs = checkIsCs(socket)
-      if (!isCs) {
-        socket.userName = ticket.name;
-        socket.join("open_ticket_users");
-        io.to("room_cs").emit('add_ticket_user', { userId: socket.userId, userName: socket.userName });
-      }
+    socket.on(on_event_open_chat_session, async (ticket) => {
+      await on_event_open_chat_session_handler(socket, ticket, checkIsCs, io, messageStore);
     });
 
-    socket.on('close_ticket', () => {
-      console.log('close_ticket ' + socket.id);
-      var isCs = checkIsCs(socket)
-      if (!isCs) {
-        socket.leave("open_ticket_users");
-        io.to("room_cs").emit('remove_ticket_user', { userId: socket.userId });
-      }
+    socket.on(on_event_close_chat_session, () => {
+      on_event_close_chat_session_handler(socket, checkIsCs);
+    });
+
+    socket.on('ping', () => {
+      console.log('user ping ' + socket.id);
     });
 
     socket.on('disconnect', () => {
-      console.log('user disconnected');
+      console.log('user disconnected ' + socket.id);
     });
   });
 
@@ -142,6 +114,75 @@ async function main() {
 
 main();
 
+async function on_event_open_chat_session_handler(socket, ticket, checkIsCs, io, messageStore) {
+  console.log('open_chat_session ' + socket.id + " ticket " + JSON.stringify(ticket));
+  var isCs = checkIsCs(socket);
+  socket.userName = ticket.name;
+  io.to(socket.id).emit(emit_event_open_chat_session_success, { userId: socket.userId, userName: socket.userName, isCs: isCs });
+
+  if (isCs) {
+    socket.join(open_chat_session_cs_room);
+    socket.join(cs_room);
+    const sockets = await io.in(open_chat_session_users_room).allSockets();
+    const tickets = new Map();
+
+    for (const socketId of sockets) {
+      var socketTicket = io.sockets.sockets.get(socketId);
+      tickets.set(socketTicket.userId, {
+        userId: socketTicket.userId,
+        userName: socketTicket.userName,
+      });
+    }
+    var arrayTickets = Array.from(tickets.values());
+    io.to(socket.id).emit(emit_event_open_session_users, arrayTickets);
+    console.log('emit_event_open_session_users ' + JSON.stringify(tickets));
+  } else {
+    socket.join(open_chat_session_users_room);
+    io.to(cs_room).emit(emit_event_add_chat_session_user, { userId: socket.userId, userName: socket.userName });
+
+    socket.join(get_user_room_by_id(socket.userId));
+
+    //latest 50 msg + isMore (query 51 msg) -> client call API to get more msg
+    var count = 50;
+    const msgList = await messageStore.findMessagesForUser(socket.userId, count + 1);
+    io.to(socket.id).emit(emit_event_chat_history, {
+      msgList: msgList.slice(0, count + 1),
+      isMore: msgList.length > count
+    });
+  }
+}
+
+function on_event_close_chat_session_handler(socket, checkIsCs) {
+  console.log('close_chat_session ' + socket.id);
+  var isCs = checkIsCs(socket);
+  if (isCs) {
+    socket.leave(open_chat_session_cs_room);
+  } else {
+    socket.leave(open_chat_session_users_room);
+  }
+}
+
+function on_event_chat_handler(checkIsCs, socket, msg, messageStore, io) {
+  var isCs = checkIsCs(socket);
+  console.log('chat ' + socket.id + " msg " + JSON.stringify(msg));
+  var roomId = socket.userId;
+  if (isCs) {
+    roomId = msg.receiverUserId;
+  }
+
+  //client msg id improve UX, check status of sending msg
+  //msg.msg_uid
+  var message = new Message(
+    socket.userId, msg.receiverUserId, msg.msg, roomId
+  );
+
+  messageStore.saveMessage(message);
+  io.to(get_user_room_by_id(roomId)).to(cs_room).emit(emit_event_new_chats, [message]);
+}
+
+function get_user_room_by_id(userId) {
+  return "room_user_" + userId;
+}
 // token cs 
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo5OTk5OTksInJvbGUiOiJjcyIsImlhdCI6MTYzODg4NTkwOCwiZXhwIjoxNjQyODA5OTA4fQ.jnfKxpR0fxwdCg-rEqpJc1xIWNiaozEq4a0WkRGK624
 // tokenBuyer
